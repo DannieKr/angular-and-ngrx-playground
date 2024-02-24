7c79268505b9389182ec5cec00839876fe95428d
KROESCHD <dannie.kroesche@stud.hs-hannover.de>
2024-02-24 16:45:37 +0100

    Added styling for register and added formBuilder

--- mediumclone_angular/src/app/auth/components/register/register.component.ts
+++ mediumclone_angular/src/app/auth/components/register/register.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'mc-register',
     templateUrl: './register.component.html',
-    standalone: true
+    imports: [
+        RouterLink,
+        ReactiveFormsModule,
+    ],
+    standalone: true,
 })
 export class RegisterComponent {
+    form = this.formBuilder.group({
+        username: ['', Validators.required],
+        email: ['', Validators.required],
+        password: ['', Validators.required],
+    });
+    constructor(private formBuilder: FormBuilder) {}
+
+    onSubmit() {
+        console.log('form', this.form.getRawValue());
+    }
 }
