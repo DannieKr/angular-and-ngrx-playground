a2c841be02101207e881a85c1a41a6107ed10e18
KROESCHD <dannie.kroesche@stud.hs-hannover.de>
2024-02-24 16:21:38 +0100

    implemented register module

--- mediumclone_angular/src/app/app.component.ts
+++ mediumclone_angular/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  standalone: true,
-  imports: [RouterOutlet],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    standalone: true,
+    imports: [RouterOutlet],
 })
 export class AppComponent {
 }
